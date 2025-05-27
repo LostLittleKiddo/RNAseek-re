cb6144cf3baec7cc5d710c44fecb8a295017f4b3
Benj <BenCode.git@gmail.com>
2025-05-27 16:22:05 -0600

    results page

--- rsa/urls.py
+++ rsa/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('results/', views.results, name='results'),
+    path('results/<uuid:project_id>/', views.results, name='result'),
 ]
